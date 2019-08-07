e8291f1b513c3dbc3de69a104da6747eecea27c5
GraceUmutesi <tesigrace98@gmail.com>
2019-08-07 20:16:23 +0200

    update changes fromm chatbox

--- js/script.js
+++ js/script.js
@@ -2,30 +2,40 @@ function cycleBackgrounds() {
     var index = 0;
     $imageEls = $('.containere .slide'); // Get the images to be cycled.
     setInterval(function () {
-    // Get the next index.  If at end, restart to the beginning.
-    index = index + 1 < $imageEls.length ? index + 1 : 0;
-    // Show the next
-    $imageEls.eq(index).addClass('show');
-    // Hide the previous
-    $imageEls.eq(index - 1).removeClass('show');
-}, 8000);
-};
-// Document Ready.
-$(function () {
-cycleBackgrounds();
-});
-$(document).ready(function(){
-    $("#plus").click(function(){
-        $("#more").toggle();
-    });
-    $('#start-chat').click(function(){
+        // Get the next index.  If at end, restart to the beginning.
+        index = index + 1 < $imageEls.length ? index + 1 : 0;
+        // Show the next
+        $imageEls.eq(index).addClass('show');
+        // Hide the previous
+        $imageEls.eq(index - 1).removeClass('show');
+    }, 8000);
+   };
+   // Document Ready.
+   $(function () {
+    cycleBackgrounds();
+   });
+   $(document).ready(function(){
+       $("#plus").click(function(){
+           $("#more").toggle();
+
+       });
+       $('#start-chat').click(function(){
         $('#chat').show()
         $('#chatbox').hide()
-    });
-    $('#newChat').click(function(){
+        });
+        $('#newChat').click(function(){
         $('#chat').hide()
         $('.info').show()
+        // $('#more').hide()
         $('#chatbox').show()
-    });
-});
+        });
+        $("#submit").click(function(){
+            Message=$("#message").val();
+            if(Message!==""){
+                $("div#talk").append("<p>" + Message + "</p>");
+            }
+
+        });
+   });
+
    
