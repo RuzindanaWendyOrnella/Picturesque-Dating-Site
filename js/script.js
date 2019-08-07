674110c704ac967c0a70a9814ed5e3a25acc2eec
GraceUmutesi <tesigrace98@gmail.com>
2019-08-07 15:48:49 +0200

    adding a chatbox

--- js/script.js
+++ js/script.js
@@ -2,21 +2,30 @@ function cycleBackgrounds() {
     var index = 0;
     $imageEls = $('.containere .slide'); // Get the images to be cycled.
     setInterval(function () {
-        // Get the next index.  If at end, restart to the beginning.
-        index = index + 1 < $imageEls.length ? index + 1 : 0;
-        // Show the next
-        $imageEls.eq(index).addClass('show');
-        // Hide the previous
-        $imageEls.eq(index - 1).removeClass('show');
-    }, 8000);
-   };
-   // Document Ready.
-   $(function () {
-    cycleBackgrounds();
-   });
-   $(document).ready(function(){
-       $("#more").onclick(function(){
-           $("#more-buttons").toggle();
-
-       });
-   });
+    // Get the next index.  If at end, restart to the beginning.
+    index = index + 1 < $imageEls.length ? index + 1 : 0;
+    // Show the next
+    $imageEls.eq(index).addClass('show');
+    // Hide the previous
+    $imageEls.eq(index - 1).removeClass('show');
+}, 8000);
+};
+// Document Ready.
+$(function () {
+cycleBackgrounds();
+});
+$(document).ready(function(){
+    $("#plus").click(function(){
+        $("#more").toggle();
+    });
+    $('#start-chat').click(function(){
+        $('#chat').show()
+        $('#chatbox').hide()
+    });
+    $('#newChat').click(function(){
+        $('#chat').hide()
+        $('.info').show()
+        $('#chatbox').show()
+    });
+});
+   
