0a595f969905c68b0b3fcb3e1837773872d0cdba
Angelica007 <angelicanagahire@gmail.com>
2019-08-09 00:18:45 +0200

    Modified the files

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-var bgImageArray = ["https://slack-redir.net/link?url=https%3A%2F%2Fst3.depositphotos.com%2F1017228%2F19282%2Fi%2F1600%2Fdepositphotos_192820048-stock-photo-portrait-of-a-happy-young.jpg", "https://www.singleblackmale.org/wp-content/uploads/2014/06/African-American-Couple-Kissing1.jpg", "https://image.shutterstock.com/image-photo/love-air-beautiful-young-african-260nw-400220989.jpg", "https://imgix.bustle.com/uploads/image/2019/4/4/d65394f1-de0c-4018-8374-72f88edfabd5-2t4a0735.JPG?w=970&h=546&fit=crop&crop=faces&auto=format&q=70", "https://ocdn.eu/images/pulscms/YjE7MDA_/2d07ba45dd9110fd86dbb868aa11131d.jpg"];
+var bgImageArray = ["https://slack-redir.net/link?url=https%3A%2F%2Fst3.depositphotos.com%2F1017228%2F19282%2Fi%2F1600%2Fdepositphotos_192820048-stock-photo-portrait-of-a-happy-young.jpg", "https://www.singleblackmale.org/wp-content/uploads/2014/06/African-American-Couple-Kissing1.jpg", "https://ak6.picdn.net/shutterstock/videos/7263226/thumb/1.jpg", "https://imgix.bustle.com/uploads/image/2019/4/4/d65394f1-de0c-4018-8374-72f88edfabd5-2t4a0735.JPG?w=970&h=546&fit=crop&crop=faces&auto=format&q=70", "https://st.depositphotos.com/2931363/4105/i/450/depositphotos_41053929-stock-photo-beautiful-young-african-couple.jpg"];
 base = "",
 secs = 6;
 bgImageArray.forEach(function(img){
@@ -19,3 +19,32 @@ function backgroundSequence() {
         }
     }
     backgroundSequence();
+$(document).ready(function(){
+    // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+$("#lin").click(function(){
+    var firstName= $('#fname').val();
+    var lastName = $('#lname').val();
+    var userName = $('#uname').val();
+    var email = $('#email').val();
+    var password = $('#pwd').val();
+    var confirm = $('#r-pwd').val();
+    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+        // Handle Errors here.
+        alert("ERROR");
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // [START_EXCLUDE]
+        if (errorCode == 'auth/weak-password') {
+          alert('The password is too weak.');
+        } else {
+          alert(errorMessage);
+        }
+        console.log(error);
+        // [END_EXCLUDE]
+      }).success(function(json) { 
+            console.log(json); 
+      });
+      // [END createwithemail]
+    });
+});
